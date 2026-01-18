7b722801571f5f34df17d37c3ad08614fac5e084
aravind-j <aravindjayaramanagri@gmail.com>
2026-01-19 01:51:46 +0530

    Add copyright text

--- R/inflen.quant.R
+++ R/inflen.quant.R
@@ -1,3 +1,19 @@
+### This file is part of 'LEAVcore' package for R.
+
+### Copyright (C) 2024-2026, ICAR-NBPGR.
+#
+# rpcss is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# rpcss is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  https://www.r-project.org/Licenses/
 
 #' Compute Information Length for Quantitative Traits
 #'
