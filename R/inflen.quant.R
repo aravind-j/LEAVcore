ae95a2876b5fcd38dedc182b6d3a18a5380cc4ef
aravind-j <aravindjayaramanagri@gmail.com>
2026-05-17 23:26:11 +0530

    Fix header

--- R/inflen.quant.R
+++ R/inflen.quant.R
@@ -2,12 +2,12 @@
 
 ### Copyright (C) 2024-2026, ICAR-NBPGR.
 #
-# rpcss is free software: you can redistribute it and/or modify
+# LEAVcore is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
-# rpcss is distributed in the hope that it will be useful,
+# LEAVcore is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
