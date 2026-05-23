a669f8f2d973a5e58115a2e2fe5b32734e44d84a
aravind-j <aravindjayaramanagri@gmail.com>
2026-05-23 09:09:27 +0530

    Fix reference and add copyright

--- R/round_preserve_sum.R
+++ R/round_preserve_sum.R
@@ -1,8 +1,25 @@
+### This file is part of 'LEAVcore' package for R.
+
+### Copyright (C) 2024-2026, ICAR-NBPGR.
+#
+# LEAVcore is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# LEAVcore is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  https://www.r-project.org/Licenses/
+
 #' Round Numeric Values While Preserving a Target Sum
 #'
 #' Applies the Hamilton (largest remainder or Hare-Niemeyer or Vinton) rounding
-#' method \insertCite{balinski_fair_2001}{LEAVcore} to a numeric vector so that
-#' the rounded values sum to a specified target.
+#' method \insertCite{balinski_fair_2001} to a numeric vector so that the
+#' rounded values sum to a specified target.
 #'
 #' Values are first rounded down using \code{floor()}, and the remaining deficit
 #' is allocated to elements with the largest fractional parts.
@@ -14,6 +31,8 @@
 #' @return An numeric vector of the same length as \code{x}, where the elements
 #'   sum to \code{target}.
 #'
+#' @export
+#'
 #' @references
 #'
 #' \insertAllCited{}
@@ -23,7 +42,6 @@
 #'
 #' round_preserve_sum(c(10.4, 10.4, 10.2), target = 32)
 #'
-#' @export
 round_preserve_sum <- function(x, target = round(sum(x))) {
   y <- floor(x)
   deficit <- target - sum(y)
